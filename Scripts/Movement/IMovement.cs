10f89aa823a6addd42aa1968c048c4c85272aa7e
Alam Rodriguez <alamgrodriguez@gmail.com>
2023-05-08 20:37:55 -0600

    Animations and gravity
    - Implemented in-game "Run" and "Idle" animations of the player.
    - Gravity force added to the Player body.

--- Scripts/Movement/IMovement.cs
+++ Scripts/Movement/IMovement.cs
@@ -2,6 +2,6 @@ public interface IMovement
 {
 	float MoveSpeed { get; set; }
 	float JumpForce { get; set; }
-	void TranslateWithArrows(float deltaTime);
+	void TranslateHorizontally(float deltaTime);
 	void Jump();
 }
--- Scripts/Movement/Movement.cs
+++ Scripts/Movement/Movement.cs
@@ -6,9 +6,25 @@ public partial class Movement : CharacterBody2D, IMovement
 	public float MoveSpeed { get; set; } = 100f;
 	[Export]
 	public float JumpForce { get; set; } = -400f;
-	
-	// Start
-	public override void _Ready()
+	[Export]
+	private AnimationTree _animationTree;
+	[Export]
+	private Sprite2D _sprite;
+
+	private bool _isIdling = false;
+	private bool _isRunning = false;
+	private Vector2 _velocity;
+	private const string ANIMATION_PARAMS_ROOT = "parameters/conditions/";
+	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public enum AnimationParams
+	{
+		Idle,
+		Run,
+	}
+
+    // Start
+    public override void _Ready()
 	{
 		base._Ready();	
 	}
@@ -16,22 +32,27 @@ public partial class Movement : CharacterBody2D, IMovement
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-	}
+        _isRunning = Mathf.Abs(Axis.X) > 0f;
+        _isIdling = !_isRunning;
+		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.Idle, _isIdling);
+		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.Run, _isRunning);
+		_sprite.FlipH = Axis.X < 0f || (Axis.X <= 0f && _sprite.FlipH);
+    }
+
 	// FixedUpdate
 	public override void _PhysicsProcess(double delta) 
 	{
 		base._PhysicsProcess(delta);
-        TranslateWithArrows((float)delta);
-	}
-
-	public void TranslateWithArrows(float deltaTime)
-	{
-		var axis = Axis;
-		Velocity = axis * MoveSpeed;
+        //TranslateHorizontally((float)delta);
+		_velocity = Velocity;
+        _velocity.Y += _gravity * (float)delta;
+        TranslateHorizontally((float)delta);
+		Velocity = _velocity;
 		MoveAndSlide();
-	}
+    }
 
-	private Vector2 Axis => new Vector2(Input.GetAxis("Left", "Right"), Input.GetAxis("Up", "Down"));
+    public void TranslateHorizontally(float deltaTime) => _velocity.X = Axis.X * MoveSpeed;
+    private Vector2 Axis => new Vector2(Input.GetAxis("Left", "Right"), Input.GetAxis("Up", "Down"));
 
 	public void Jump()
 	{
