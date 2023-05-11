329fba4e2def6d5a746be6b13022655c68b3842c
Alam Rodriguez <alamgrodriguez@gmail.com>
2023-05-10 21:01:38 -0600

    Jump and animations
    - Implemented Jump logic
    - Implemented animations for Jump and Falling

--- Scripts/Movement/Movement.cs
+++ Scripts/Movement/Movement.cs
@@ -13,6 +13,8 @@ public partial class Movement : CharacterBody2D, IMovement
 
 	private bool _isIdling = false;
 	private bool _isRunning = false;
+	private bool _isJumping = false;
+	private bool _isFalling = false;
 	private Vector2 _velocity;
 	private const string ANIMATION_PARAMS_ROOT = "parameters/conditions/";
 	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -21,6 +23,9 @@ public partial class Movement : CharacterBody2D, IMovement
 	{
 		Idle,
 		Run,
+		Jump,
+		Falling,
+		IsOnFloor,
 	}
 
     // Start
@@ -32,10 +37,13 @@ public partial class Movement : CharacterBody2D, IMovement
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-        _isRunning = Mathf.Abs(Axis.X) > 0f;
+        _isRunning = Mathf.Abs(Axis.X) > 0f && IsOnFloor();
         _isIdling = !_isRunning;
 		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.Idle, _isIdling);
 		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.Run, _isRunning);
+		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.Jump, _isJumping);
+		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.Falling, _isFalling);
+		_animationTree.Set(ANIMATION_PARAMS_ROOT + AnimationParams.IsOnFloor, IsOnFloor());
 		_sprite.FlipH = Axis.X < 0f || (Axis.X <= 0f && _sprite.FlipH);
     }
 
@@ -46,7 +54,16 @@ public partial class Movement : CharacterBody2D, IMovement
         //TranslateHorizontally((float)delta);
 		_velocity = Velocity;
         _velocity.Y += _gravity * (float)delta;
+		_isFalling = _velocity.Y > 0f;
+		_isJumping = !IsOnFloor() && !_isFalling;
         TranslateHorizontally((float)delta);
+        if (Input.IsActionPressed("Jump") && IsOnFloor())
+		{
+            _velocity.Y = JumpForce;
+            _isIdling = false;
+			_isRunning = false;
+            _isJumping = true;
+        }
 		Velocity = _velocity;
 		MoveAndSlide();
     }
